67557ed2db791236c77966e12fde6d6b467e0997
Matt <mzyupc@163.com>
2023-11-05 11:10:38 +0800

    添加TODO

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ Send Time: {send_time}
 
 Group: {group}
 
-Group ID: {group_id}
+From ID: {from_id}
 '''
+
+last_file_name = ''
 
 
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
@@ -31,25 +33,24 @@ def text_forward(msg):
     account_b = get_account_b_user()
     if not account_b:
         return
-    # if msg.user.remarkName == account_b.remarkName:
-    #     # todo distribute the message
-    #     return
+    if msg.user.remarkName == account_b.remarkName:
+        distribute_text(msg)
+        return
     if msg.type != 'Text':
         itchat.send_raw_msg(msg.msgType, msg.oriContent if '' != msg.oriContent else msg.content, toUserName=account_b.userName)
         itchat.send(forward_msg_format.format(sender=msg.user.remarkName,
                                               message=msg.type,
                                               group='None',
-                                              group_id='None',
+                                              from_id=msg.user.userName,
                                               send_time=get_now()),
                     toUserName=account_b.userName)
     else:
         itchat.send(forward_msg_format.format(sender=msg.user.remarkName,
                                               message=msg.text,
                                               group='None',
-                                              group_id='None',
+                                              from_id=msg.user.userName,
                                               send_time=get_now()),
                     toUserName=account_b.userName)
-
 
 
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isGroupChat=True)
@@ -62,23 +63,22 @@ def group_text_forward(msg):
     account_b = get_account_b_user()
     if not account_b:
         return
-    # if msg.user.remarkName == account_b.remarkName:
-    #     # todo distribute the message
-    #     return
+    if msg.user.remarkName == account_b.remarkName:
+        return
 
     if msg.type != 'Text':
         itchat.send_raw_msg(msg.msgType, msg.oriContent if '' != msg.oriContent else msg.content, toUserName=account_b.userName)
         itchat.send(forward_msg_format.format(sender=msg.actualNickName,
                                               message=msg.type,
                                               group=get_group_name(msg),
-                                              group_id=msg.user.userName,
+                                              from_id=msg.user.userName,
                                               send_time=get_now()),
                     toUserName=account_b.userName)
     else:
         itchat.send(forward_msg_format.format(sender=msg.actualNickName,
                                               message=msg.text,
                                               group=get_group_name(msg),
-                                              group_id=msg.user.userName,
+                                              from_id=msg.user.userName,
                                               send_time=get_now()),
                     toUserName=account_b.userName)
 
@@ -94,14 +94,14 @@ def media_forward(msg):
     download_path = 'download/' + msg.fileName
     msg.download(download_path)
     account_b = get_account_b_user()
-    # if msg.user.remarkName == account_b.remarkName:
-    #     # todo
-    #     return
+    if msg.user.remarkName == account_b.remarkName:
+        distribute_media(msg)
+        return
     itchat.send('@%s@%s' % ('img' if msg.type == 'Picture' else 'fil', download_path), account_b.userName)
     itchat.send(forward_msg_format.format(sender=msg.user.remarkName,
                                           message=msg.type,
                                           group='None',
-                                          group_id='None',
+                                          from_id=msg.user.userName,
                                           send_time=get_now()),
                 toUserName=account_b.userName)
 
@@ -110,23 +110,48 @@ def media_forward(msg):
 def group_media_forward(msg):
     """
     Forward files, including images, received by account A to account B.
-    If the received message is from account B, then record the file paths for future use.
+    If the received message is from account B, then save the file path for future use.
     :param msg:
     :return:
     """
     download_path = 'download/' + msg.fileName
     msg.download(download_path)
     account_b = get_account_b_user()
-    # if msg.user.remarkName == account_b.remarkName:
-    #     # todo
-    #     return
+    if msg.user.remarkName == account_b.remarkName:
+        return
     itchat.send('@%s@%s' % ('img' if msg.type == 'Picture' else 'fil', download_path), account_b.userName)
     itchat.send(forward_msg_format.format(sender=msg.actualNickName,
                                           message=msg.type,
                                           group=get_group_name(msg),
-                                          group_id=msg.user.userName,
+                                          from_id=msg.user.userName,
                                           send_time=get_now()),
                 toUserName=account_b.userName)
+
+
+def distribute_text(msg):
+    """
+    todo
+    消息以 '/' 开头指的是要执行的命令
+        1. /search xxx (根据关键词搜索好友或者群聊), 返回内容有 remarkName, nickName, actualNickName, userName 等
+
+    消息包含引用, 则需要解析引用中的内容来分发消息
+        1. 引用中是图片, 则消息内容应该是 From ID, 将 last_file_name 指定的文件发送给 From ID 表示的用户
+        2. 引用中没有图片, 提取引用中的 From ID, 将消息发送给 From ID 表示的用户
+    :param msg:
+    :return:
+    """
+
+
+def distribute_media(msg):
+    """
+    save the file path for future use
+    :param msg:
+    :return:
+    """
+    download_path = 'download/' + msg.fileName
+    msg.download(download_path)
+    global last_file_name
+    last_file_name = download_path
 
 
 def get_group_name(msg):
